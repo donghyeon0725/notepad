b72225a342375933aac50a6a0c74b71362e9fc3d
donghyeon0725 <ehdgus5015@gmail.com>
2021-09-01 17:17:30 +0900

    fix

--- DB/Reservation.sql
+++ DB/Reservation.sql
@@ -11,8 +11,7 @@ CREATE TABLE `Reservation`
 (
  `reservation_id`     serial NOT NULL ,
  `member_id`          bigint unsigned NOT NULL ,
- `date`               date NOT NULL ,
- `time`               time NOT NULL ,
+ `clinic_time_id`               bigint unsigned NOT NULL ,
  `etc`                varchar(1000) NULL ,
  `phone_area_code`    varchar(3) NOT NULL ,
  `phone_number`       varchar(4) NOT NULL ,
@@ -21,7 +20,10 @@ CREATE TABLE `Reservation`
 PRIMARY KEY (`reservation_id`),
 KEY `fkIdx_45` (`member_id`),
 CONSTRAINT `FK_44` FOREIGN KEY `fkIdx_45` (`member_id`) REFERENCES `Member` (`member_id`)
-);
+)
+KEY `fkIdx_45_1` (`clinic_time_id`),
+CONSTRAINT `FK_44` FOREIGN KEY `fkIdx_45_1` (`clinic_time_id`) REFERENCES `Clinic Time` (`clinic_time_id`)
+;
 
 
 
